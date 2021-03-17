13bf1eab803609e76300c899bd977ea0e7ff0911
Chase Bennett <lordiieepwns@gmail.com>
2021-03-17 16:38:01 -0400

    resorting

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express')
 const path = require("path")
 const app = express()
 const port = 3000
-
+app.use(express.static(path.join(__dirname,"public")));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/ToDoList.html'));
 });
 
-var test
 app.listen(port, () => {
     console.log("Example app listening at http://localhost:${port}")
   })
